44f6c5775340d54f614335383b46aa791cff2a89
PleaseInsertNameHere <73995049+PleaseInsertNameHere@users.noreply.github.com>
2024-08-09 21:40:02 +0200

    cleanup main

--- src/main/java/xyz/syodo/cheat/SyoCheat.java
+++ src/main/java/xyz/syodo/cheat/SyoCheat.java
@@ -3,40 +3,38 @@ package xyz.syodo.cheat;
 import cn.nukkit.Server;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.plugin.PluginManager;
 import lombok.Getter;
 import xyz.syodo.database.redis.SyoRedis;
 import xyz.syodo.cheat.utils.CheatPlayerManager;
 
 public class SyoCheat extends PluginBase {
 
-	@Getter
-	public static final String prefix = "§r§8» §b§lSyo§cCheat§r§8│§c ";
-
+	@Getter public static final String prefix = "§r§8» §b§lSyo§cCheat§r§8│§c ";
+	@Getter private static SyoRedis redis;
 	private static Plugin plugin;
-	@Getter
-	private static SyoRedis redis;
+
+	public static Plugin get() {
+		return plugin;
+	}
 
 	@Override
 	public void onLoad() {
+		plugin = this;
 		redis = new SyoRedis("jointick");
 	}
 
 	@Override
 	public void onEnable() {
-		
-		plugin = this;
-		registerListeners();
-		getServer().getLogger().info("§aSyoCheat §ev" + this.getDescription().getVersion() + " §aenabled!");
-	
-	}
-	
-	public static Plugin get() {
-		return plugin;
+		this.registerListeners();
+
+		this.getServer().getLogger().info("§aSyoCheat §ev" + this.getDescription().getVersion() + " §aenabled!");
 	}
 	
 	public void registerListeners() {
-		Server.getInstance().getPluginManager().registerEvents(new CheatPlayerManager(), plugin);
+		PluginManager pluginManager = Server.getInstance().getPluginManager();
+
+		pluginManager.registerEvents(new CheatPlayerManager(), plugin);
 	}
-	
 	
 }
