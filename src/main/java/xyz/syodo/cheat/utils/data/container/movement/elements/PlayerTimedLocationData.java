5313de525dbcdf060434d3bb13a33e2d0df19619
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-08-11 10:08:57 +0200

    Create PlayerTimedLocationData.java

---
+++ src/main/java/xyz/syodo/cheat/utils/data/container/movement/elements/PlayerTimedLocationData.java
@@ -0,0 +1,93 @@
+package xyz.syodo.cheat.utils.data.container.movement.elements;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.player.PlayerTeleportEvent;
+import cn.nukkit.level.Location;
+import cn.nukkit.math.Vector3f;
+import cn.nukkit.network.protocol.PlayerAuthInputPacket;
+import lombok.Setter;
+import xyz.syodo.cheat.utils.data.CheatCheck;
+import xyz.syodo.cheat.utils.data.CheatResponse;
+import xyz.syodo.cheat.utils.data.Container;
+import xyz.syodo.cheat.utils.data.Data;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class PlayerTimedLocationData extends Data {
+
+    @Setter
+    private static int REMEMBER_LOCATIONS = 20;
+
+    @Setter
+    private static double ALLOWED_SINGLE_DISTANCE = 4.2d;
+    @Setter
+    private static double ALLOWED_AVERAGE_DISTANCE = 3.8d;
+
+    private Long teleported = System.currentTimeMillis();
+
+    final List<Location> locations = new ArrayList<>();
+
+    public PlayerTimedLocationData(Container container) {
+        super(container);
+    }
+
+    public CheatResponse addLocation() {
+        if(System.currentTimeMillis() - teleported < 500) {
+            locations.clear();
+            return new CheatResponse(CheatCheck.OTHER);
+        }
+        locations.addLast(getContainer().getPlayer().getPlayer().getLocation().clone().setY(0));
+        if(locations.size() > REMEMBER_LOCATIONS) {
+            locations.removeFirst();
+        }
+        return doCheck();
+    }
+
+    @Override
+    public CheatResponse doCheck() {
+        CheatResponse response = new CheatResponse(CheatCheck.SPEED);
+        if(getContainer().getPlayer().getPlayer().getAllowFlight()) return response;
+        if(locations.size() > 10) {
+            double highest = 0;
+            double lowest = Integer.MAX_VALUE;
+            double average = 0;
+            Iterator<Location> iterator = locations.iterator();
+            Location latest = iterator.next();
+            while(iterator.hasNext()) {
+                Location location = iterator.next();
+                double distance = location.distance(latest);
+                if(distance > highest) highest = distance;
+                if(distance < lowest) lowest = distance;
+                average += distance;
+                latest = location;
+                if(!iterator.hasNext()) {
+                    if(distance > ALLOWED_SINGLE_DISTANCE) {
+                        response.setCheating(true);
+                        response.getCheck().setCheatpoints((int) (response.getCheck().getCheatpoints() * (distance/ALLOWED_SINGLE_DISTANCE)));
+                        if(distance > ALLOWED_SINGLE_DISTANCE*1.5) {
+                            getContainer().getPlayer().getPlayer().teleport(latest, PlayerTeleportEvent.TeleportCause.PLUGIN);
+                        }
+                    }
+                }
+            }
+            average /= locations.size();
+            response.getMetaData().put("lowest", lowest);
+            response.getMetaData().put("highest", highest);
+            response.getMetaData().put("average", average);
+            if(average > ALLOWED_AVERAGE_DISTANCE) {
+                response.setCheating(true);
+                for(int i = 0; i < (REMEMBER_LOCATIONS / 3) * 2; i++) locations.removeFirst();
+            }
+        }
+        return  response;
+    }
+
+    public void teleported() {
+        this.teleported = System.currentTimeMillis();
+        locations.clear();
+    }
+
+}
