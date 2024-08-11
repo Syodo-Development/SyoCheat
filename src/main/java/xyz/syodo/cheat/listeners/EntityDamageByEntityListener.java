62bae39d72c192c8963505a6a04d4d106a60bfec
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-08-11 10:08:59 +0200

    Update EntityDamageByEntityListener.java

--- src/main/java/xyz/syodo/cheat/listeners/EntityDamageByEntityListener.java
+++ src/main/java/xyz/syodo/cheat/listeners/EntityDamageByEntityListener.java
@@ -3,19 +3,28 @@ package xyz.syodo.cheat.listeners;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import xyz.syodo.cheat.utils.CheatPlayer;
 import xyz.syodo.cheat.utils.CheatPlayerManager;
+import xyz.syodo.cheat.utils.data.CheatResponse;
 
 public class EntityDamageByEntityListener implements Listener {
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if(event.getEntity() instanceof Player player) {
+        if(event.getDamager() instanceof Player player) {
             CheatPlayer cheatPlayer = CheatPlayerManager.getPlayer(player);
             if(cheatPlayer.getCombatContainer().getHitCooldownData().doCheck().isCheating()) {
                 event.setCancelled();
+            }
+            if(!event.isCancelled()) {
+                CheatResponse response = cheatPlayer.getCombatContainer().getReachData().addReachElement(event);
+                if(response.isCheating()) {
+                    event.setCancelled();
+                    cheatPlayer.addResponse(response);
+                }
             }
         }
     }
