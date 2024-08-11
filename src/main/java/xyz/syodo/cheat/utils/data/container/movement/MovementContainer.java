51ab0e09b06be81cf3a287ec89958ace53fcedc9
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-08-11 12:30:45 +0200

    Update MovementContainer.java

--- src/main/java/xyz/syodo/cheat/utils/data/container/movement/MovementContainer.java
+++ src/main/java/xyz/syodo/cheat/utils/data/container/movement/MovementContainer.java
@@ -5,12 +5,14 @@ import xyz.syodo.cheat.utils.CheatPlayer;
 import xyz.syodo.cheat.utils.data.Container;
 import xyz.syodo.cheat.utils.data.container.movement.elements.PlayerAuthInputData;
 import xyz.syodo.cheat.utils.data.container.movement.elements.PlayerTimedLocationData;
+import xyz.syodo.cheat.utils.data.container.movement.elements.VelocityData;
 
 @Getter
 public class MovementContainer extends Container {
 
     private final PlayerAuthInputData playerAuthInputData = new PlayerAuthInputData(this);
     private final PlayerTimedLocationData playerTimedLocationData = new PlayerTimedLocationData(this);
+    private final VelocityData velocityData = new VelocityData(this);
 
     public MovementContainer(CheatPlayer player) {
         super(player);
