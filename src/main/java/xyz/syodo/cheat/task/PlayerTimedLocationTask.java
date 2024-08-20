541734d26e569fdebc2b82fef7984b3d55838817
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2024-08-20 08:42:53 +0200

    Update PlayerTimedLocationTask.java

--- src/main/java/xyz/syodo/cheat/task/PlayerTimedLocationTask.java
+++ src/main/java/xyz/syodo/cheat/task/PlayerTimedLocationTask.java
@@ -6,6 +6,6 @@ import xyz.syodo.cheat.utils.CheatPlayerManager;
 public class PlayerTimedLocationTask extends Task {
     @Override
     public void onRun(int i) {
-        CheatPlayerManager.players.values().forEach(player -> player.addResponse(player.getMovementContainer().getPlayerTimedLocationData().addLocation()));
+        CheatPlayerManager.getPlayers().values().forEach(player -> player.addResponse(player.getMovementContainer().getPlayerTimedLocationData().addLocation()));
     }
 }
